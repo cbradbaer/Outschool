f8b026c66404c8fca528001a071ad62c61a78a30
cbradbaer <cbbaer@hotmail.com>
2020-07-02 16:37:04 -0700

    Add files via upload

---
+++ Section2/Lesson 6/VehicleConsoleApp/VehicleConsoleApp/Vehicle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VehicleConsoleApp
+{
+    public class Vehicle
+    {
+        //private string _maker;
+
+
+        //public string maker
+        //{
+        //    get { return _maker; }
+        //    set {_maker = value; }
+        //}
+
+        public string maker { get; set; }
+        public string model { get; set; }
+        public int year { get; set; }
+        public int topSpeed { get; set; }
+
+
+        public Vehicle()
+        {
+
+
+        }
+
+        public Vehicle(string _maker, string _model, int _year)
+        {
+            maker = _maker;
+            model = _model;
+            year = _year;
+        }
+
+
+    }
+}
