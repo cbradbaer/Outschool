758d29380b0299692d49bbec4bbc8283c4c5e5d7
cbradbaer <cbbaer@hotmail.com>
2020-07-07 19:29:36 -0700

    Add files via upload

---
+++ Section2/Lesson 7/VinylVaultApp/View/HomePage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VinylVaultApp.ViewModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace VinylVaultApp.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePage : ContentPage
+    {
+
+        AlbumViewModel avm;
+
+        public HomePage()
+        {
+            InitializeComponent();
+            avm = new AlbumViewModel();
+            BindingContext = avm;
+        }
+
+        private void btnName_Clicked(object sender, EventArgs e)
+        {
+            avm.CollectionName = "Carys Rock Collection";
+
+        }
+    }
+}
