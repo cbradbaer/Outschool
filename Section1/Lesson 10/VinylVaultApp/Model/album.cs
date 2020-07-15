4e226f0dbe18dd8d676e7fb0f90ef2265167b222
cbradbaer <cbbaer@hotmail.com>
2020-07-15 11:29:09 -0700

    Add files via upload

---
+++ Section1/Lesson 10/VinylVaultApp/Model/album.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace VinylVaultApp.Model
+{
+    public class album
+    {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Location { get; set; }
+        public string ThumbImage { get; set; }
+        public string Genre { get; set; }
+
+        public album(string _Title, string _Artist, string _Location, string _ThumbImage, string _Genre)
+        {
+            Title = _Title;
+            Artist = _Artist;
+            Location = _Location;
+            ThumbImage = _ThumbImage;
+            Genre = _Genre;
+        }
+
+
+    }
+}
---
+++ Section1/Lesson 10/VinylVaultApp/ViewModel/AlbumViewModel.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Threading.Tasks;
+using VinylVaultApp.Model;
+
+namespace VinylVaultApp.ViewModel
+{
+    public class AlbumViewModel : INotifyPropertyChanged
+    {
+        public album SelectedAlbum;
+        private string _CollectionName;
+        public string CollectionName
+        {
+            get
+            {
+                return _CollectionName;
+            }
+            set
+            {
+                _CollectionName = value;
+                RaisePropertyChanged("CollectionName");
+            }
+        }
+
+        public List<album> albumList;
+
+        public async Task<bool> GetCollection()
+        {
+            if (albumList == null)
+            {
+                albumList = new List<album>();
+                albumList.Add(new album("The Doors", "The Doors", "Box 1", "Album1.jpg", "Rock"));
+                albumList.Add(new album("Escape", "Journey", "Box 2", "Album3.jpg", "Rock"));
+                albumList.Add(new album("Volunteers", "Jefferson Airplane", "Box 3", "Album2.jpg", "Rock"));
+            }
+
+            return true;
+
+
+        }
+
+        public bool DeleteAlbum(album alb)
+        {
+            albumList.Remove(alb);
+            return true;
+
+        }
+
+        public bool InsertAlbum(album alb)
+        {
+            albumList.Add(alb);
+            return true;
+
+        }
+
+        public bool UpdateAlbum(album alb)
+        {
+            SelectedAlbum.Title = alb.Title;
+            SelectedAlbum.Artist = alb.Artist;
+            SelectedAlbum.Location = alb.Location;
+            SelectedAlbum.ThumbImage = alb.ThumbImage;
+            SelectedAlbum.Genre = alb.Genre;
+            return true;
+        }
+
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged(string prop)
+        {
+            if (PropertyChanged != null) { PropertyChanged(this, new PropertyChangedEventArgs(prop)); }
+        }
+
+
+
+
+    }
+}
