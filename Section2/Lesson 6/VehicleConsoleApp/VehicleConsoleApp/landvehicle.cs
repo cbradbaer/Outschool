f86f0cc99a5f4c4e9cbc233f262358fea91ee8c1
cbradbaer <cbbaer@hotmail.com>
2020-08-13 17:37:38 -0700

    Lesson 6 code

---
+++ Section2/Lesson 6/VehicleConsoleApp/VehicleConsoleApp/landvehicle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VehicleConsoleApp
+{
+    public class landvehicle : vehicle
+    {
+        public string color { get; set; }
+        public int numWheels { get; set; }
+    }
+}
---
+++ Section2/Lesson 6/VehicleConsoleApp/VehicleConsoleApp/vehicle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VehicleConsoleApp
+{
+    public class vehicle
+    {
+        //private string _maker;
+
+        //public string maker
+        //{
+        //    get { 
+        //        return _maker; 
+        //    }
+        //    set { 
+        //        _maker = value; 
+        //    }
+        //}
+
+        public string maker { get; set; }
+        public string model { get; set; }
+        public int year { get; set; }
+
+    }
+}
