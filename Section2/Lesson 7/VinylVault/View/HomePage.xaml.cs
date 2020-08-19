9c0d763c0662f36b507e0568e31529418f27196c
cbradbaer <cbbaer@hotmail.com>
2020-08-18 17:51:10 -0700

    Lesson 7 code

---
+++ Section2/Lesson 7/VinylVault/View/HomePage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VinylVault.ViewModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace VinylVault.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePage : ContentPage
+    {
+
+        AlbumViewModel avm;
+
+        public HomePage()
+        {
+            InitializeComponent();
+
+            avm = new AlbumViewModel();
+
+            BindingContext = avm;
+
+
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            avm.CollectionName = "Cary Classic Collection";
+
+
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+            avm.CollectionName = "Some Other Name";
+
+        }
+    }
+}
