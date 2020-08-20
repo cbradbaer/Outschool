8a568cf467283652b298ac0be6bcca41b1551726
cbradbaer <cbbaer@hotmail.com>
2020-08-19 17:59:02 -0700

    Lesson 8 Code

---
+++ Section1/Lesson 8/VinylVault/View/HomePage.xaml.cs
@@ -0,0 +1,82 @@
+﻿using Acr.UserDialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VinylVault.Model;
+using VinylVault.ViewModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace VinylVault.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePage : ContentPage
+    {
+
+        AlbumViewModel avm;
+        List<AlbumModel> albumList;
+
+        public HomePage()
+        {
+            InitializeComponent();
+            avm = new AlbumViewModel();
+            BindingContext = avm;
+
+        }
+
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            avm.CollectionName = "Cary's Classic Collection";
+            await RefreshListView();
+
+        }
+
+
+        public async Task<bool> RefreshListView()
+        {
+            listView.ItemsSource = null;
+            await avm.GetAlbums();
+            albumList = avm.AlbumList;
+            listView.ItemsSource = albumList;
+            return true;
+        }
+
+
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            PromptConfig pc = new PromptConfig();
+            pc.Title = "Collection Name";
+            pc.Message = "Choose a Collection Name";
+
+            var result = await UserDialogs.Instance.PromptAsync(pc);
+
+            if(result.Ok && result.Text != "")
+            {
+
+                avm.CollectionName = result.Text;
+            }
+
+            
+
+        }
+
+        private async void listView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem != null)
+            {
+                avm.SelectedAlbum = (AlbumModel)e.SelectedItem;
+                await DisplayAlert(avm.SelectedAlbum.Title, avm.SelectedAlbum.Artist, "OK");
+
+            }
+
+
+
+        }
+    }
+}
