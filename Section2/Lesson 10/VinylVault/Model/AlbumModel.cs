90dd5a1e02c8b3654da9fdbc241989662259298a
cbradbaer <cbbaer@hotmail.com>
2020-08-27 17:45:11 -0700

    Lesson 10 Code

---
+++ Section2/Lesson 10/VinylVault/Model/AlbumModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VinylVault.Model
+{
+    public class AlbumModel
+    {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Genre { get; set; }
+        public string Location { get; set; }
+        public string ThumbImage { get; set; }
+        public bool IsPlayable { get; set; }
+
+        public AlbumModel(string _Title, string _Artist, string _Genre, string _Location, string _ThumbImage)
+        {
+            Title = _Title;
+            Artist = _Artist;
+            Genre = _Genre;
+            Location = _Location;
+            ThumbImage = _ThumbImage;
+
+        }
+
+
+
+    }
+}
---
+++ Section2/Lesson 10/VinylVault/ViewModel/AlbumViewModel.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Common;
+using System.Text;
+using System.Threading.Tasks;
+using VinylVault.Model;
+using Xamarin.Forms;
+
+namespace VinylVault.ViewModel
+{
+    public class AlbumViewModel : INotifyPropertyChanged
+    {
+        public List<AlbumModel> albumList;
+        public AlbumModel selectedAlbum;
+
+
+
+
+        private string _CollectionName;
+        public string CollectionName
+        {
+            get
+            {
+                return _CollectionName;
+
+            }
+            set 
+            {
+                _CollectionName = value;
+                RaisePropertyChanged("CollectionName");
+            }
+
+        }
+
+
+        public async Task<bool> GetAlbums()
+        {
+            if (albumList == null)
+            {
+                albumList = new List<AlbumModel>();
+                albumList.Add(new AlbumModel("Escape", "Journey", "Rock", "Box1", "https://picsum.photos/300"));
+                albumList.Add(new AlbumModel("Exit", "Rush", "Rock", "Box2", "https://picsum.photos/301")); 
+                albumList.Add(new AlbumModel("Rumors", "Fleetwood Mac", "Rock", "Box1", "https://picsum.photos/302"));
+                albumList.Add(new AlbumModel("Escape", "Journey", "Rock", "Box1", "https://picsum.photos/300"));
+                albumList.Add(new AlbumModel("Exit", "Rush", "Rock", "Box2", "https://picsum.photos/301"));
+                albumList.Add(new AlbumModel("Rumors", "Fleetwood Mac", "Rock", "Box1", "https://picsum.photos/302"));
+                albumList.Add(new AlbumModel("Escape", "Journey", "Rock", "Box1", "https://picsum.photos/300"));
+                albumList.Add(new AlbumModel("Exit", "Rush", "Rock", "Box2", "https://picsum.photos/301"));
+                albumList.Add(new AlbumModel("Rumors", "Fleetwood Mac", "Rock", "Box1", "https://picsum.photos/302"));
+                albumList.Add(new AlbumModel("Escape", "Journey", "Rock", "Box1", "https://picsum.photos/300"));
+                albumList.Add(new AlbumModel("Exit", "Rush", "Rock", "Box2", "https://picsum.photos/301"));
+                albumList.Add(new AlbumModel("Rumors", "Fleetwood Mac", "Rock", "Box1", "https://picsum.photos/302"));
+            }
+
+            return true;
+
+        }
+
+
+        public bool DeleteAlbum(AlbumModel alb)
+        {
+            albumList.Remove(alb);
+            return true;
+
+        }
+
+
+        public bool InsertAlbum(AlbumModel alb)
+        {
+            albumList.Add(alb);
+            return true;
+
+        }
+
+        public bool UpdateAlbum(AlbumModel alb)
+        {
+            selectedAlbum.Title = alb.Title;
+            selectedAlbum.Artist = alb.Artist;
+            selectedAlbum.Genre = alb.Genre;
+            selectedAlbum.Location = alb.Location;
+            selectedAlbum.ThumbImage = alb.ThumbImage;
+
+            return true;
+
+        }
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged(string prop)
+        {
+            if (PropertyChanged != null) { PropertyChanged(this, new PropertyChangedEventArgs(prop)); }
+        }
+
+
+
+
+
+    }
+}
