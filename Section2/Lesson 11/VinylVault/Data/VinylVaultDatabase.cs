26b56f8f793d6d61cf112fe708b971c52f4f2ea2
cbradbaer <cbbaer@hotmail.com>
2020-09-01 17:41:44 -0700

    Lesson 11 Code

---
+++ Section2/Lesson 11/VinylVault/Data/VinylVaultDatabase.cs
@@ -0,0 +1,62 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using VinylVault.Model;
+
+namespace VinylVault.Data
+{
+    public class VinylVaultDatabase
+    {
+        SQLiteAsyncConnection database;
+
+
+        public VinylVaultDatabase(string dbPath)
+        {
+
+            database = new SQLiteAsyncConnection(dbPath);
+            database.CreateTableAsync<AlbumModel>().Wait();
+
+
+
+
+        }
+
+
+        public async Task<List<AlbumModel>> GetAlbumsAsync()
+        {
+            return await database.Table<AlbumModel>().ToListAsync();
+
+        }
+
+        public async Task<int> InsertAlbum(AlbumModel am)
+        {
+            await database.InsertAsync(am);
+            return 1;
+
+        }
+
+        public async Task<int> DeleteAlbum(AlbumModel am)
+        {
+            await database.ExecuteScalarAsync<AlbumModel>("DELETE FROM [AlbumModel] WHERE Title = ?", am.Title);
+            return 1;
+        }
+
+        public async Task<int> UpdateAlbum(AlbumModel am)
+        {
+
+            await database.ExecuteScalarAsync<AlbumModel>("UPDATE [AlbumModel] SET Artist = ?, Genre = ?, Location = ?, ThumbImage = ? WHERE Title = ?",
+                            am.Artist, am.Genre, am.Location, am.ThumbImage, am.Title );
+
+            return 1;
+
+        }
+
+
+            
+
+
+
+    }
+}
---
+++ Section2/Lesson 11/VinylVault/ViewModel/AlbumViewModel.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Common;
+using System.Text;
+using System.Threading.Tasks;
+using VinylVault.Model;
+using Xamarin.Forms;
+
+namespace VinylVault.ViewModel
+{
+    public class AlbumViewModel : INotifyPropertyChanged
+    {
+        public List<AlbumModel> albumList;
+        public AlbumModel selectedAlbum;
+
+
+
+
+        private string _CollectionName;
+        public string CollectionName
+        {
+            get
+            {
+                return _CollectionName;
+
+            }
+            set 
+            {
+                _CollectionName = value;
+                RaisePropertyChanged("CollectionName");
+            }
+
+        }
+
+
+        public async Task<bool> GetAlbums()
+        {
+
+            albumList = await App.Database.GetAlbumsAsync();
+
+            if (albumList.Count == 0)
+            {
+                albumList = new List<AlbumModel>();
+                await App.Database.InsertAlbum(new AlbumModel("The Doors", "The Doors", "Rock", "Box 1", "https://picsum.photos/300"));
+                await App.Database.InsertAlbum(new AlbumModel("Volunteers", "Jefferson Airplane", "Rock", "Box 1", "https://picsum.photos/310"));
+                await App.Database.InsertAlbum(new AlbumModel("Escape", "Journey", "Rock", "Box 2", "https://picsum.photos/302"));
+                await App.Database.InsertAlbum(new AlbumModel("Exit", "Tangerine Dream", "Electronic", "Box 2", "https://picsum.photos/303"));
+                await App.Database.InsertAlbum(new AlbumModel("Rumors", "Fleetwood Mac", "Rock", "Box 2", "https://picsum.photos/304"));
+                await App.Database.InsertAlbum(new AlbumModel("LA Woman", "The Doors", "Rock", "Box 1", "https://picsum.photos/305"));
+                await App.Database.InsertAlbum(new AlbumModel("Moving Pictures", "Rush", "Rock", "Box 2", "https://picsum.photos/306"));
+            }
+
+            return true;
+
+        }
+
+
+        public async Task<bool> DeleteAlbum(AlbumModel alb)
+        {
+            //albumList.Remove(alb);
+
+            await App.Database.DeleteAlbum(alb);
+            return true;
+
+        }
+
+
+        public async Task<bool> InsertAlbum(AlbumModel alb)
+        {
+            //albumList.Add(alb);
+            await App.Database.InsertAlbum(alb);
+            return true;
+
+        }
+
+        public async Task<bool> UpdateAlbum(AlbumModel alb)
+        {
+            //selectedAlbum.Title = alb.Title;
+            //selectedAlbum.Artist = alb.Artist;
+            //selectedAlbum.Genre = alb.Genre;
+            //selectedAlbum.Location = alb.Location;
+            //selectedAlbum.ThumbImage = alb.ThumbImage;
+
+            await App.Database.UpdateAlbum(alb);
+            return true;
+
+        }
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged(string prop)
+        {
+            if (PropertyChanged != null) { PropertyChanged(this, new PropertyChangedEventArgs(prop)); }
+        }
+
+
+
+
+
+    }
+}
