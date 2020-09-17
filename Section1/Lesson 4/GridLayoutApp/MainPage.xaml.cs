ad41fcb94383ebdc4b29361bd9bcb81df1507656
cbradbaer <cbbaer@hotmail.com>
2020-09-16 17:29:48 -0700

    lesson 4 code

---
+++ Section1/Lesson 4/GridLayoutApp/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GridLayoutApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Switch_Toggled(object sender, ToggledEventArgs e)
+        {
+            if (e.Value == true)
+                img.IsVisible = true;
+            else
+                img.IsVisible = false;
+
+            //  img.IsVisible = e.Value;
+
+        }
+    }
+}
