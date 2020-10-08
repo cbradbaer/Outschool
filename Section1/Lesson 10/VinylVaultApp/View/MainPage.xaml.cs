1a4b05f2f9465d92f4d77fadbeb9df7a2844b94a
cbradbaer <cbbaer@hotmail.com>
2020-10-07 17:34:38 -0700

    Lesson 10 code

---
+++ Section1/Lesson 10/VinylVaultApp/View/MainPage.xaml.cs
@@ -0,0 +1,121 @@
+﻿using Acr.UserDialogs;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VinylVaultApp.Model;
+using VinylVaultApp.ViewModel;
+using Xamarin.Forms;
+
+namespace VinylVaultApp.View
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        AlbumViewModel avm;
+        List<AlbumModel> albumList;
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            avm = new AlbumViewModel();
+
+            BindingContext = avm;
+
+
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            avm.CollectionName = "Carys Rock Collection";
+
+            await RefreshListView();
+        }
+
+
+        public async Task<bool> RefreshListView()
+        {
+
+            listView.ItemsSource = null;
+            await avm.GetAlbums();
+            albumList = avm.albumList;
+            listView.ItemsSource = albumList;
+            return true;
+        }
+
+
+
+
+        private async void btnName_Clicked(object sender, EventArgs e)
+        {
+            PromptConfig pc = new PromptConfig();
+            pc.Title = "Collection Name";
+            pc.Message = "Choose a Collection Name";
+
+            var result = await UserDialogs.Instance.PromptAsync(pc);
+
+            if (result.Ok && result.Text != "")
+            {
+                avm.CollectionName = result.Text;
+
+            }
+  
+        }
+
+        private async void listView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem != null)
+            {
+                avm.selectedAlbum = (AlbumModel)e.SelectedItem;
+                DetailPage dp = new DetailPage(avm, false);
+                await Navigation.PushModalAsync(dp);
+
+            }
+
+
+        }
+
+        private void srchBar_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(string.IsNullOrEmpty(e.NewTextValue))
+            {
+                listView.ItemsSource = albumList;
+            }
+            else
+            {
+                listView.ItemsSource = albumList.Where(x => x.Title.ToLower().Contains(e.NewTextValue.ToLower()));
+
+            }
+
+
+
+        }
+
+        private async void MenuItem_Clicked(object sender, EventArgs e)
+        {
+            MenuItem mi = (MenuItem)sender;
+            AlbumModel alb = (AlbumModel)mi.CommandParameter;
+            if (await DisplayAlert("Delete Album", alb.Title, "Yes", "No"))
+            {
+                await avm.DeleteAlbum(alb);
+                await RefreshListView();
+
+
+            }
+        }
+
+        private async void btnAdd_Clicked(object sender, EventArgs e)
+        {
+            DetailPage dp = new DetailPage(avm, true);
+            await Navigation.PushModalAsync(dp);
+        }
+    }
+}
---
+++ Section1/Lesson 10/VinylVaultApp/ViewModel/AlbumViewModel.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Threading.Tasks;
+using VinylVaultApp.Model;
+
+namespace VinylVaultApp.ViewModel
+{
+    public class AlbumViewModel : INotifyPropertyChanged
+    {
+        private string _CollectionName;
+        public List<AlbumModel> albumList;
+        public AlbumModel selectedAlbum;
+
+        public string CollectionName
+        {
+            get
+            {
+                return _CollectionName;
+
+            }
+            set
+            {
+                _CollectionName = value;
+                RaisePropertyChanged("CollectionName");
+            }
+
+
+        }
+
+
+        public async Task<bool> GetAlbums()
+        {
+            if (albumList == null)
+            {
+                albumList = new List<AlbumModel>();
+                albumList.Add(new AlbumModel("LA Woman", "The Doors", "Rock", "https://picsum.photos/200", "Box 1"));
+                albumList.Add(new AlbumModel("Escape", "Journey", "Rock", "https://picsum.photos/201", "Box 2"));
+                albumList.Add(new AlbumModel("Rumors", "Fleetwood Mac", "Rock", "https://picsum.photos/202", "Box 3"));
+            }
+
+            return true;
+
+
+        }
+
+        public async Task<bool> DeleteAlbum(AlbumModel alb)
+        {
+
+            albumList.Remove(alb);
+            return true;
+
+        }
+
+        public async Task<bool> InsertAlbum(AlbumModel alb)
+        {
+
+            albumList.Add(alb);
+            return true;
+
+        }
+
+        public async Task<bool> UpdateAlbum(AlbumModel alb)
+        {
+
+            selectedAlbum.Title = alb.Title;
+            selectedAlbum.Artist = alb.Artist;
+            selectedAlbum.Genre = alb.Genre;
+            selectedAlbum.Location = alb.Location;
+            selectedAlbum.ThumbImage = alb.ThumbImage;
+
+            return true;
+
+        }
+
+
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged(string prop)
+        {
+            if (PropertyChanged != null) { PropertyChanged(this, new PropertyChangedEventArgs(prop)); }
+        }
+
+    }
+}
