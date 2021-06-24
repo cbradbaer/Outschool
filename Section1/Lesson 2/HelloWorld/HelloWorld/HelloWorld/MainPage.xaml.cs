16db14ab3b636a731b8fef80cce2c24f94964630
cbradbaer <cbbaer@hotmail.com>
2021-06-23 18:19:08 -0700

    Lesson 2

---
+++ Section1/Lesson 2/HelloWorld/HelloWorld/HelloWorld/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HelloWorld
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOK_Clicked(object sender, EventArgs e)
+        {
+            lblHello.Text = "Hello " + entryName.Text;
+        }
+    }
+}
