2614497a4cfa06527c79b77c621948ebd08311d6
cbradbaer <cbbaer@hotmail.com>
2021-06-30 18:26:02 -0700

    lesson 4 grid layout

---
+++ Section1/Lesson 4/GridLayout/GridLayout/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GridLayout
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Switch_Toggled(object sender, ToggledEventArgs e)
+        {
+            if (e.Value == true)
+                img.IsVisible = true;
+            else
+                img.IsVisible = false;
+
+
+
+        }
+    }
+}
