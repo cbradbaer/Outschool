a67601d0e6113bee07c5922b96d5924866349e0c
cbradbaer <cbbaer@hotmail.com>
2021-07-12 18:16:38 -0700

    Section 1 Lesson 7 Collection App 1

---
+++ Section1/Lesson 7/VinylVault/VinylVault/VinylVault/View/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VinylVault.ViewModel;
+using Xamarin.Forms;
+
+namespace VinylVault
+{
+    public partial class MainPage : ContentPage
+    {
+        AlbumViewModel avm;
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+            avm = new AlbumViewModel();
+
+            BindingContext = avm;
+
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            avm.CollectionName = "Cary's Classic Collection";
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            avm.CollectionName = "Old Time Rock n Roll Collection";
+        }
+    }
+}
