887d957ad5ac0ef9351d8c2c01aa63a64307850c
cbradbaer <cbbaer@hotmail.com>
2021-08-09 18:57:42 -0700

    section 2 Lesson 9 vv3

---
+++ Section2/Lesson 9/VinylVault/VinylVault/VinylVault/View/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿
+using Acr.UserDialogs;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VinylVault.ViewModel;
+using Xamarin.Forms;
+
+namespace VinylVault
+{
+    public partial class MainPage : ContentPage
+    {
+
+        AlbumViewModel avm;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            avm = new AlbumViewModel();
+            BindingContext = avm;
+        }
+
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            avm.CollectionName = "Cary's Rock Collection";
+            listview.ItemsSource = null;
+
+            avm.GetAlbums();
+            listview.ItemsSource = avm.AlbumList;
+        }
+
+        private async void btnCollectionName_Clicked(object sender, EventArgs e)
+        {
+            PromptConfig pg = new PromptConfig();
+            pg.Title = "Collection Name";
+            pg.Message = "Choose a Collection Name";
+
+            var result = await UserDialogs.Instance.PromptAsync(pg);
+
+            if(result.Ok == true && result.Text != "")
+            {
+
+                avm.CollectionName = result.Text;
+            }
+
+        }
+    }
+}
---
+++ Section2/Lesson 9/VinylVault/VinylVault/VinylVault/ViewModel/AlbumViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using VinylVault.Model;
+
+namespace VinylVault.ViewModel
+{
+    public class AlbumViewModel : INotifyPropertyChanged
+    {
+        private string _CollectionName;
+
+        public string CollectionName
+        {
+            get
+            {
+                return _CollectionName;
+            }
+
+            set
+            {
+                _CollectionName = value;
+                RaisePropertyChanged("CollectionName");
+                
+
+            }
+
+        }
+
+
+        public List<Album> AlbumList;
+
+
+        public bool GetAlbums()
+        {
+            if(AlbumList == null)
+            {
+                AlbumList = new List<Album>();
+                AlbumList.Add(new Album("The Doors", "The Doors", "Box 1", "", "Rock"));
+                AlbumList.Add(new Album("Volunteers", "Jefferson Airplane", "Box 1", "", "Rock"));
+                AlbumList.Add(new Album("Escape", "Journey", "Box 2", "", "Rock"));
+                AlbumList.Add(new Album("Exit", "Tangerine Dream", "Box 2", "", "Electronic"));
+                AlbumList.Add(new Album("Rumors", "Fleetwood Mac", "Box 2", "", "Rock"));
+                AlbumList.Add(new Album("LA Woman", "The Doors", "Box 1", "", "Rock"));
+                AlbumList.Add(new Album("Moving Pictures", "Rush", "Box 2", "", "Rock"));
+            }
+
+            return true;
+
+        }
+
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged(string prop)
+        {
+            if (PropertyChanged != null) { PropertyChanged(this, new PropertyChangedEventArgs(prop)); }
+        }
+
+
+
+    }
+}
